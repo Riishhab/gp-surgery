48e7e336fbea53715441ca6bd61ef21c58d179bf
charleyyyyyyy74 <101596967+charleyyyyyyy74@users.noreply.github.com>
2023-04-08 22:38:43 +0100

    uppdates
    update on back, appointment table and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import './styles.scss';
 import Header from './header';
+import Back from './Back';
+import Footer from './footer';
+import AppointmentTable from './AppointmentTable';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Back />
+      <AppointmentTable />
+      <Footer />
     </div>
   );
 }
