3bff3abbf6a3cb5ff8f5b22312bacab625c39dd3
CTD003 <craigdowling48@gmail.com>
2023-04-23 01:47:50 +0100

    Updated Code
    Footer now stuck to bottom

--- src/AppointmentBooking.js
+++ src/AppointmentBooking.js
@@ -1,4 +1,3 @@
-import './App.css';
 import './styles.scss';
 import React, { useState } from "react";
 
@@ -16,20 +15,45 @@ function AppointmentBooking() {
     <label class="govuk-label" for="appointment-date">
       Date
     </label>
-    <input class="govuk-input" id="appointment-date" name="appointment-date" type="date" min={today}></input>
+    <input class="govuk-input govuk-input--width-20" id="appointment-date" name="appointment-date" type="date" min={today}></input>
   </div>
   <div class="govuk-form-group">
     <label class="govuk-label" for="appointment-time">
       Time
     </label>
-    <input class="govuk-input" id="appointment-time" name="appointment-time" type="time"></input>
+    <select class="govuk-select govuk-input--width-20" id="appointment-time" name="appointment-time">
+        <option value="08:30">08:30</option>
+        <option value="09:00">09:00</option>
+        <option value="09:30">09:30</option>
+        <option value="10:00">10:00</option>
+        <option value="10:30">10:30</option>
+        <option value="11:00">11:00</option>
+        <option value="11:30">11:30</option>
+        <option value="12:00">12:00</option>
+        <option value="12:30">12:30</option>
+        <option value="13:00">13:00</option>
+        <option value="13:30">13:30</option>
+        <option value="14:00">14:00</option>
+        <option value="14:30">14:30</option>
+        <option value="15:00">15:00</option>
+        <option value="15:30">15:30</option>
+        <option value="16:00">16:00</option>
+        <option value="16:30">16:30</option>
+        <option value="17:00">17:00</option>
+        <option value="17:30">17:30</option>
+        <option value="18:00">18:00</option>
+        <option value="18:30">18:30</option>
+    </select>
   </div>
   <div class="govuk-form-group">
-    <label class="govuk-label" for="address-town">
+    <label class="govuk-label" for="appointment-time">
       Notes
     </label>
-    <input class="govuk-input govuk-!-width-two-thirds" id="address-town" name="address-town" type="text" ></input>
+    <input class="govuk-input govuk-input--width-20" id="appointment-time" name="appointment-time" type="text" ></input>
   </div>
+  <button class="govuk-button" data-module="govuk-button" type="submit">
+  Confirm and Book
+</button>
 </fieldset>
     </form>
   );
