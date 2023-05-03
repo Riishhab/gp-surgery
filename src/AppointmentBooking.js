87e51745c1f34fc2fdb6add1d2c1767898259b0b
CTD003 <craigdowling48@gmail.com>
2023-05-03 12:45:45 +0100

    Craig
    PHP

--- src/AppointmentBooking.js
+++ src/AppointmentBooking.js
@@ -1,5 +1,9 @@
 import './styles.scss';
 import React from "react";
+import footer from './footer';
+import jq from 'jquery'
+import $ from 'jquery';
+import header from './header';
 
 function AppointmentBooking() {
     const today = new Date(); //Gets todays date
@@ -7,9 +11,32 @@ function AppointmentBooking() {
     const nextWeek = new Date(); //Gets todays date
     nextWeek.setDate(today.getDate() + 7); //adds 7 days onto todays date (exactly one week)
     const nextWeekString = nextWeek.toISOString().split("T")[0]; //formats the date as a string using ISO 8601 format, T splits it between date and time then [0] selects the date
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Button Pressed")
+
+    const formData = {
+      date: $('#appointment-date').val(),
+      time: $('#appointment-time').val(),
+      notes: $('#appointment-notes').val(),
+    };
+    var url_appointment = 'http://localhost:4000/bookappointment.php';
+        jq.ajax({
+            type: "POST",
+            url: url_appointment,
+            mode: "no-core",
+            data: formData,
+            success(data) {
+                console.log(data);
+            }
+        });
+  };
+
   return (
-      <div class="AppointmentBooking">
-    <form method="post" action="/book-appointment">
+      <div>
+      <header />
+    <form class="AppointmentBooking">
     <h1 class="govuk-heading-xl">
   <span class="govuk-caption-xl">Appointment Booking</span>
   Book an Appointment with us
@@ -55,16 +82,17 @@ function AppointmentBooking() {
     </select>
   </div>
   <div class="govuk-form-group">
-    <label class="govuk-label" for="appointment-time">
+    <label class="govuk-label" for="appointment-notes">
       If necessary please provide some notes
     </label>
-    <textarea class="govuk-textarea govuk-input--width-20" id="appointment-time" name="appointment-time" rows="5"></textarea>
+    <textarea class="govuk-textarea govuk-input--width-20" id="appointment-notes" name="appointment-notes" rows="5"></textarea>
   </div>
-  <button class="govuk-button" data-module="govuk-button" type="submit">
+  <button class="govuk-button" data-module="govuk-button" type="submit" onClick={handleSubmit}>
   Confirm and Book
 </button>
 </fieldset>
     </form>
+    <footer />
     </div>
   );
 }
