1bab7198d1c72efa1bb17df0a06bffca1e553bf6
charleyyyyyyy74 <101596967+charleyyyyyyy74@users.noreply.github.com>
2023-05-06 16:48:06 +0100

    Update appointment.js
    updated the appointment.js page for admin

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,8 +1,7 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.scss";
 
 function Appointment() {
-  const [NHSNumber, setNhsNumber] = useState("");
   const [appointments, setAppointments] = useState([]);
 
   useEffect(() => {
@@ -12,115 +11,69 @@ function Appointment() {
       .catch((error) => console.error(error));
   }, []);
 
-  
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const response = await fetch(
-      `/api/appointments?nhsNumber=${NHSNumber}`
-    );
-    const data = await response.json();
-    setAppointments(data);
-  };
-
-  const handleDelete = async (appointmentNumber) => {
-    await fetch(`/api/appointments/${appointmentNumber}`, { method: "DELETE" });
-    setAppointments((currentAppointments) =>
-      currentAppointments.filter(
-        (appointment) => appointment.appointmentNumber !== appointmentNumber
-      )
-    );
+  const handleDelete = (id) => {
+    fetch(`/api/appointments/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setAppointments(
+          appointments.filter((appointment) => appointment.id !== id)
+        );
+      })
+      .catch((error) => console.error(error));
   };
 
   return (
-    <>
-      <div className="govuk-width-container ">
-        <h2 className="govuk-cookie-banner__heading govuk-heading-m">
-          GP Appointments
-        </h2>
-        <form onSubmit={handleSubmit}>
-          <label className="govuk-label govuk-label--3" htmlFor="nhsNumber">
-            NHS Number:
-          </label>
-          <input
-            className="govuk-label"
-            for="width-8"
-            type="text"
-            id="nhsNumber"
-            value={NHSNumber}
-            onChange={(event) => setNhsNumber(event.target.value)}
-          />
-          <button
-            className="govuk-button"
-            data-module="govuk-button"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
-        <table className="govuk-table">
-          <thead className="govuk-table__head">
-            <tr className="govuk-table__row">
-              <th scope="col" className="govuk-table__header">
-                Appointment Number
-              </th>
-              <th scope="col" className="govuk-table__header">
-                NHS Number
-              </th>
-              <th scope="col" className="govuk-table__header">
-                Medical License
-              </th>
-              <th scope="col" className="govuk-table__header">
-                Date of Appointment
-              </th>
-              <th scope="col" className="govuk-table__header">
-                Time of Appointment
-              </th>
-              <th scope="col" className="govuk-table__header">
-                Appointment Notes
-              </th>
-              <th scope="col" className="govuk-table__header">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody className="govuk-table__body">
-            {appointments.map((appointment) => (
-              <tr
-                className="govuk-table__row"
-                key={appointment.appointmentNumber}
+    <table className="govuk-table">
+      <caption className="govuk-table__caption govuk-table__caption--m">
+        GP appointment
+      </caption>
+      <thead className="govuk-table__head">
+        <tr className="govuk-table__row">
+          <th scope="col" className="govuk-table__header">
+            Appointment Number
+          </th>
+          <th scope="col" className="govuk-table__header">
+            NHS Number
+          </th>
+          <th scope="col" className="govuk-table__header">
+            Medical License
+          </th>
+          <th scope="col" className="govuk-table__header">
+            Notes
+          </th>
+          <th scope="col" className="govuk-table__header">
+            Actions
+          </th>
+        </tr>
+      </thead>
+      <tbody className="govuk-table__body">
+        {appointments.map((appointment) => (
+          <tr className="govuk-table__row" key={appointment.id}>
+            <td className="govuk-table__cell">
+              {appointment.AppointmentNumber}
+            </td>
+            <td className="govuk-table__cell">
+              {appointment.NHSNumber}
+            </td>
+            <td className="govuk-table__cell">{appointment.medicalLicenseNumber}</td>
+            <td className="govuk-table__cell">{appointment.appointentNotes}</td>
+            <td className="govuk-table__cell">
+              <button
+                className="govuk-button govuk-button--warning"
+                data-module="govuk-button"
+                onClick={() => handleDelete(appointment.id)}
               >
-                <td className="govuk-table__row">
-                  {appointment.appointmentNumber}
-                </td>
-                <td className="govuk-table__row">{appointment.NHSNumber}</td>
-                <td className="govuk-table__row">
-                  {appointment.medicalLicenseNumber}
-                </td>
-                <td className="govuk-table__row">
-                  {appointment.dateOfAppointment}
-                </td>
-                <td className="govuk-table__row">
-                  {appointment.timeOfAppointment}
-                </td>
-                <td className="govuk-table__row">
-                  {appointment.appointmentNotes}
-                </td>
-                <td className="govuk-table__row">
-                  <button
-                    className="govuk-button govuk-button--warning"
-                    data-module="govuk-button"
-                    onClick={() => handleDelete(appointment.appointmentNumber)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
 export default Appointment;
+
